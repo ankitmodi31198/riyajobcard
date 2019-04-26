223ae1a278590f16731fcc017cb7533ee9bc9cb5
Ankit Modi <ankitmodi31198@gmail.com>
2019-04-26 15:32:24 +0530

    make it better

--- src/main/java/modal/ConnectionDb.java
+++ src/main/java/modal/ConnectionDb.java
@@ -9,7 +9,7 @@ public class ConnectionDb {
 		        Connection con=null;  
 		        try{  
 		            Class.forName("com.mysql.jdbc.Driver");  
-		            con= DriverManager.getConnection("jdbc:mysql://us-cdbr-iron-east-03.cleardb.net/heroku_d6605bcfbc2eb2a","bfb48045b38f3a","b6bf2b78");
+		            con= DriverManager.getConnection("jdbc:mysql://localhost:3306/jcps","root","");
 		        }catch(Exception e){System.out.println(e);}  
 		        return con; 
 		    }
